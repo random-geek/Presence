7ccd9376df249f404314d352e6a53fe24556b010
random-geek <35757396+random-geek@users.noreply.github.com>
2018-07-13 21:11:30 -0700

    Add readme and screenshots

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Presence
 Colour-enhancing shaders for Minetest
+
+This is my attempt at subtly improving Minetest graphics without modifying core engine code. If you have any suggestions for future improvement, feel free to contact me, as I know little about the Minetest graphics code.
+
+## Installation
+Rename the `/client/shaders` directory to something else (or delete it entirely). Then copy the `shaders` folder to the `client` folder. The shaders will take effect after leaving and rejoining the game.
+
+## Credits
+All shaders are identical to or are derivative works of the original shaders for Minetest, found [here](https://github.com/minetest/minetest/tree/master/client/shaders). All due credit goes to the original author(s) of this code.
---
+++ shaders/nodes_shader/opengl_vertex.glsl
@@ -0,0 +1,157 @@
+uniform mat4 mWorldViewProj;
+uniform mat4 mWorld;
+
+// Color of the light emitted by the sun.
+uniform vec3 dayLight;
+uniform vec3 eyePosition;
+uniform float animationTimer;
+
+varying vec3 vPosition;
+varying vec3 worldPosition;
+
+varying vec3 eyeVec;
+varying vec3 lightVec;
+varying vec3 tsEyeVec;
+varying vec3 tsLightVec;
+varying float area_enable_parallax;
+
+// Color of the light emitted by the light sources.
+const vec3 artificialLight = vec3(1.04, 1.04, 1.04);
+
+// random-geek's colour definitions, for use with colour tinting.
+const vec3 sunEffect = vec3(1.0, 0.8, 0.4);
+const vec3 orange = vec3(1.0, 0.89, 0.75);
+const vec3 cyan = vec3(0.75, 0.89, 1.0);
+
+const float e = 2.718281828459;
+const float BS = 10.0;
+
+// random-geek's custom daylight. Colour temperature is based on amount of light, with intermediate values (dusk and dawn) being the most orange.
+vec3 globalSun = dayLight.rgb * mix(vec3(1.0), sunEffect, -2.25 * abs(dayLight.r - 0.5) + 1);
+
+float smoothCurve(float x)
+{
+	return x * x * (3.0 - 2.0 * x);
+}
+
+
+float triangleWave(float x)
+{
+	return abs(fract(x + 0.5) * 2.0 - 1.0);
+}
+
+
+float smoothTriangleWave(float x)
+{
+	return smoothCurve(triangleWave(x)) * 2.0 - 1.0;
+}
+
+
+void main(void)
+{
+	gl_TexCoord[0] = gl_MultiTexCoord0;
+	//TODO: make offset depending on view angle and parallax uv displacement
+	//thats for textures that doesnt align vertically, like dirt with grass
+	//gl_TexCoord[0].y += 0.008;
+
+	//Allow parallax/relief mapping only for certain kind of nodes
+	//Variable is also used to control area of the effect
+#if (DRAW_TYPE == NDT_NORMAL || DRAW_TYPE == NDT_LIQUID || DRAW_TYPE == NDT_FLOWINGLIQUID)
+	area_enable_parallax = 1.0;
+#else
+	area_enable_parallax = 0.0;
+#endif
+
+
+float disp_x;
+float disp_z;
+#if (MATERIAL_TYPE == TILE_MATERIAL_WAVING_LEAVES && ENABLE_WAVING_LEAVES) || (MATERIAL_TYPE == TILE_MATERIAL_WAVING_PLANTS && ENABLE_WAVING_PLANTS)
+	vec4 pos2 = mWorld * gl_Vertex;
+	float tOffset = (pos2.x + pos2.y) * 0.001 + pos2.z * 0.002;
+	disp_x = (smoothTriangleWave(animationTimer * 23.0 + tOffset) +
+		smoothTriangleWave(animationTimer * 11.0 + tOffset)) * 0.4;
+	disp_z = (smoothTriangleWave(animationTimer * 31.0 + tOffset) +
+		smoothTriangleWave(animationTimer * 29.0 + tOffset) +
+		smoothTriangleWave(animationTimer * 13.0 + tOffset)) * 0.5;
+#endif
+
+
+#if (MATERIAL_TYPE == TILE_MATERIAL_LIQUID_TRANSPARENT || MATERIAL_TYPE == TILE_MATERIAL_LIQUID_OPAQUE) && ENABLE_WAVING_WATER
+	vec4 pos = gl_Vertex;
+	pos.y -= 2.0;
+	float posYbuf = (pos.z / WATER_WAVE_LENGTH + animationTimer * WATER_WAVE_SPEED * WATER_WAVE_LENGTH);
+	pos.y -= sin(posYbuf) * WATER_WAVE_HEIGHT + sin(posYbuf / 7.0) * WATER_WAVE_HEIGHT;
+	gl_Position = mWorldViewProj * pos;
+#elif MATERIAL_TYPE == TILE_MATERIAL_WAVING_LEAVES && ENABLE_WAVING_LEAVES
+	vec4 pos = gl_Vertex;
+	pos.x += disp_x;
+	pos.y += disp_z * 0.1;
+	pos.z += disp_z;
+	gl_Position = mWorldViewProj * pos;
+#elif MATERIAL_TYPE == TILE_MATERIAL_WAVING_PLANTS && ENABLE_WAVING_PLANTS
+	vec4 pos = gl_Vertex;
+	if (gl_TexCoord[0].y < 0.05) {
+		pos.x += disp_x;
+		pos.z += disp_z;
+	}
+	gl_Position = mWorldViewProj * pos;
+#else
+	gl_Position = mWorldViewProj * gl_Vertex;
+#endif
+
+
+	vPosition = gl_Position.xyz;
+	worldPosition = (mWorld * gl_Vertex).xyz;
+
+	// Don't generate heightmaps when too far from the eye
+	float dist = distance (vec3(0.0, 0.0, 0.0), vPosition);
+	if (dist > 150.0) {
+		area_enable_parallax = 0.0;
+	}
+
+	vec3 sunPosition = vec3 (0.0, eyePosition.y * BS + 900.0, 0.0);
+
+	vec3 normal, tangent, binormal;
+	normal = normalize(gl_NormalMatrix * gl_Normal);
+	tangent = normalize(gl_NormalMatrix * gl_MultiTexCoord1.xyz);
+	binormal = normalize(gl_NormalMatrix * gl_MultiTexCoord2.xyz);
+
+	vec3 v;
+
+	lightVec = sunPosition - worldPosition;
+	v.x = dot(lightVec, tangent);
+	v.y = dot(lightVec, binormal);
+	v.z = dot(lightVec, normal);
+	tsLightVec = normalize (v);
+
+	eyeVec = -(gl_ModelViewMatrix * gl_Vertex).xyz;
+	v.x = dot(eyeVec, tangent);
+	v.y = dot(eyeVec, binormal);
+	v.z = dot(eyeVec, normal);
+	tsEyeVec = normalize (v);
+	// Calculate color.
+	// Red, green and blue components are pre-multiplied with
+	// the brightness, so now we have to multiply these
+	// colors with the color of the incoming light.
+	// The pre-baked colors are halved to prevent overflow.
+	vec4 color;
+	// The alpha gives the ratio of sunlight in the incoming light.
+	float nightRatio = 1 - gl_Color.a;
+	
+	vec3 localSun = globalSun * mix(cyan, orange, pow(gl_Color.a * globalSun.r, 3));
+	
+	color.rgb = gl_Color.rgb * (gl_Color.a * globalSun + 
+		nightRatio * artificialLight.rgb) * 2;
+	
+	// random-geek's contrast; makes lights lighter and darks darker for better clarity.
+	float light = max(color.r, max(color.g, color.b));
+	float lightNew = 0.75*(light-0.5)/(abs(light-0.5) + 0.25) + 0.5;
+	color.rgb *= lightNew/light;
+	
+	// random-geek's colour grading; adds blue to darker areas and orange to lighter areas.
+	color.rgb *= mix(cyan, orange, pow(lightNew, 2));
+	
+	color.a = 1;
+	
+	gl_FrontColor = gl_BackColor = clamp(color, 0.0, 1.0);
+}
